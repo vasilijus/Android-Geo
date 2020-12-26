4c162e41ff33b3c4555d91e9c9369b0932023839
vasilijus <vasiliokas@gmail.com>
2020-12-26 02:24:58 +0000

    added Sign in failure bit

--- app/src/main/java/com/example/applicationgeo/MainActivity.java
+++ app/src/main/java/com/example/applicationgeo/MainActivity.java
@@ -8,10 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.applicationgeo.Models.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -43,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
         db      = FirebaseDatabase.getInstance();
         users   = db.getReference("Users");
 
+        btnSignIn.setOnClickListener( new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+                showSignInWindow();
+           }
+        });
+
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,6 +59,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void showSignInWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Sign In");
+        dialog.setMessage("Enter Email & Pass");
+
+        // Find Layout
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View sign_in_window = inflater.inflate( R.layout.sign_in_window, null );
+        dialog.setView( sign_in_window );
+
+        final MaterialEditText email = findViewById( R.id.emailField );
+        final MaterialEditText pass  = findViewById( R.id.passField );
+
+        dialog.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if(TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Enter your email addr", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+
+                if( pass.getText().toString().length() < 5 ) {
+                    Snackbar.make(root, "Enter your email addr", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword( email.getText().toString(), pass.getText().toString() )
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make( root, "Sign In Failed" + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        dialog.show();
+    }
+
 
     private void showRegisterWindow() {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
@@ -101,26 +163,26 @@ public class MainActivity extends AppCompatActivity {
 
                 // Register User | need to use Auth
                 auth.createUserWithEmailAndPassword( email.getText().toString(), pass.getText().toString() )
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                User user = new User(
-                                name.getText().toString(),
-                                email.getText().toString(),
-                                pass.getText().toString(),
-                                phone.getText().toString() );
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            User user = new User(
+                            name.getText().toString(),
+                            email.getText().toString(),
+                            pass.getText().toString(),
+                            phone.getText().toString() );
 
-                                // indexing user by field: email
-                                users.child(user.getEmail())
-                                        .setValue(user)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Snackbar.make( root, "User has been created!", Snackbar.LENGTH_SHORT ).show();
-                                    }
-                                });
-                            }
-                        });
+                            // indexing user by field: email
+                            users.child(user.getEmail())
+                                    .setValue(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Snackbar.make( root, "User has been created!", Snackbar.LENGTH_SHORT ).show();
+                                }
+                            });
+                        }
+                });
             }
         });
 
